8061dc0da12920d545b632cffc249a1bdbd4ad6f
Jikky <Jikky0110@gmail.com>
2024-03-11 12:34:35 +0900

    色々アップデート

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "verifiedRoles": ["892320750940332033"],
-  "loggingChannel": "932145437715075092",
-  "logging": ["scratch.username", "discord.tag", "discord.id", "uuid"]
-}
---
+++ src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "verifiedRoles": ["892320750940332033"],
+  "loggingChannel": "932145437715075092",
+  "projectId": "673753313",
+  "studioId": "31009600",
+  "logging": ["scratch.username", "discord.tag", "discord.id", "uuid"]
+}
